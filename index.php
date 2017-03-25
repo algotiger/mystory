9e545c0864da9c4b907c0802b09299bb99f1ea3c
Fikri <marouanefikri@gmail.com>
2017-03-25 13:01:37 +0000

    init

---
+++ index.php
@@ -0,0 +1,159 @@
+<?php
+session_start();
+if (iss)
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+		<meta name="description" content="">
+		<meta name="author" content="">
+		<link rel="icon" href="../../favicon.ico">
+
+		<title>MarocStories.com</title>
+
+		<link href="assets/css/bootstrap.min.css" rel="stylesheet">
+
+		<link href="assets/css/ie10-viewport-bug-workaround.css" rel="stylesheet">
+
+		<link href="assets/css/blog.css" rel="stylesheet">
+
+		<script src="../../assets/js/ie-emulation-modes-warning.js"></script>
+
+  	</head>
+
+  <body>
+
+  	<div class="blog-masthead navbar-fixed-top">
+  		<div class="container">
+  			<nav class="blog-nav">
+  				<a class="blog-nav-item active" href="#">Home</a>
+  				<a class="blog-nav-item" href="#">New features</a>
+  				<a class="blog-nav-item" href="#">Press</a>
+  				<a class="blog-nav-item" href="#">New hires</a>
+  				<a class="blog-nav-item" href="#">About</a>
+  			</nav>
+  		</div>
+  	</div>
+
+  	<div class="container pad10">
+  		<div class="row">
+  			<div class="col-sm-3 blog-main fixed">
+
+  				<div class="blog-post">
+  					<div class="sidebar-module">
+  						<h4><a href="#">Marouane FIKRI</a></h4>
+  						<ol class="list-unstyled">
+  							<li><a href="#">March 2014</a></li>
+  							<li><a href="#">February 2014</a></li>
+  							<li><a href="#">January 2014</a></li>
+  							<li><a href="#">December 2013</a></li>
+  							<li><a href="#">November 2013</a></li>
+  							<li><a href="#">October 2013</a></li>
+  							<li><a href="#">September 2013</a></li>
+  							<li><a href="#">August 2013</a></li>
+  							<li><a href="#">July 2013</a></li>
+  							<li><a href="#">June 2013</a></li>
+  							<li><a href="#">May 2013</a></li>
+  							<li><a href="#">April 2013</a></li>
+  						</ol>
+  					</div>
+
+
+  				</div><!-- /.blog-post -->
+
+  			</div>        
+
+  			<div class="col-sm-6 blog-main">
+
+  				<div class="blog-post">
+  					<h4 >Marouane FIKRI</h4>
+
+  					<p>This blog post shows a few different types of content that's supported and styled with Bootstrap. Basic typography, images, and code are all supported.</p>
+  					<hr>  					
+
+
+  					<h4 >Marouane FIKRI</h4>
+
+  					<p>This blog post shows a few different types of content that's supported and styled with Bootstrap. Basic typography, images, and code are all supported.</p>
+  					<hr>  					
+
+
+  					<h4 >Marouane FIKRI</h4>
+
+  					<p>This blog post shows a few different types of content that's supported and styled with Bootstrap. Basic typography, images, and code are all supported.</p>
+  					<hr>  					
+
+  					<h4 >Marouane FIKRI</h4>
+
+  					<p>This blog post shows a few different types of content that's supported and styled with Bootstrap. Basic typography, images, and code are all supported.</p>
+  					<hr>  					
+
+  					<h4 >Marouane FIKRI</h4>
+
+  					<p>This blog post shows a few different types of content that's supported and styled with Bootstrap. Basic typography, images, and code are all supported.</p>
+  					<hr>
+  					
+  					
+  				</div><!-- /.blog-post -->
+
+  			</div><!-- /.blog-main -->
+
+  			<div class="col-sm-2 col-sm-offset-1 blog-sidebar">
+  				<div class="sidebar-module sidebar-module-inset">
+  					<h4>About</h4>
+  					<p>Etiam porta <em>sem malesuada magna</em> mollis euismod. Cras mattis consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla sed consectetur.</p>
+  				</div>
+  				<div class="sidebar-module">
+  					<h4>Archives</h4>
+  					<ol class="list-unstyled">
+  						<li><a href="#">March 2014</a></li>
+  						<li><a href="#">February 2014</a></li>
+  						<li><a href="#">January 2014</a></li>
+  						<li><a href="#">December 2013</a></li>
+  						<li><a href="#">November 2013</a></li>
+  						<li><a href="#">October 2013</a></li>
+  						<li><a href="#">September 2013</a></li>
+  						<li><a href="#">August 2013</a></li>
+  						<li><a href="#">July 2013</a></li>
+  						<li><a href="#">June 2013</a></li>
+  						<li><a href="#">May 2013</a></li>
+  						<li><a href="#">April 2013</a></li>
+  					</ol>
+  				</div>
+  				<div class="sidebar-module">
+  					<h4>Elsewhere</h4>
+  					<ol class="list-unstyled">
+  						<li><a href="#">GitHub</a></li>
+  						<li><a href="#">Twitter</a></li>
+  						<li><a href="#">Facebook</a></li>
+  					</ol>
+  				</div>
+  			</div><!-- /.blog-sidebar -->
+
+  		</div><!-- /.row -->
+
+  	</div><!-- /.container -->
+
+  	<footer class="blog-footer">
+  		<p>Marocstories.com by <a href="https://twitter.com/marofikri">FIKRI</a>.</p>
+  		<p>
+  			<!--a href="#">Back to top</a-->
+  		</p>
+  	</footer>
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
+    <script src="../../dist/js/bootstrap.min.js"></script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
+</body>
+</html>
